82da61b5e488a75f9a6765d7165a17fdd7e5b69a
UnYan <415212688@qq.com>
2020-05-26 16:47:37 +0800

    '帖子回复显示小修改'

--- src/main/java/com/a/controller/ReplyController.java
+++ src/main/java/com/a/controller/ReplyController.java
@@ -8,6 +8,7 @@ import com.a.repository.ReplyRepository;
 import com.a.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +27,8 @@ public class ReplyController {
     UserRepository userRepository ;
     @Autowired
     ArticleRepository articleRepository;
-    @PostMapping("/reply")
-    public String reply(@RequestParam("articleid") String articleid,
+    @PostMapping("/reply/a{id}")
+    public String replyA(@RequestParam("articleid") String articleid,
                         @RequestParam("rContent") String rContent,
                         HttpServletRequest request,
                         //String[] rContent,
@@ -41,8 +42,9 @@ public class ReplyController {
             System.out.println("0");
             return "redirect:/main";
         }*/
-        if(rContent == null)
+        if(rContent == null || rContent.length() == 0 )
             return "redirect:/main";
+        System.out.println(rContent);
         reply.content = rContent;
         reply.userid = user.id;
         reply.likes = 0;
@@ -54,15 +56,37 @@ public class ReplyController {
         return "redirect:/main";
 
     }
+    @PostMapping("/reply/r{id}")
+    public String replyR(@PathVariable("id") String replyid,
+                         @RequestParam("articleid") String articleid,
+                        @RequestParam("rContent") String rContent,
+                        HttpServletRequest request,
+                        //String[] rContent,
+                        HttpSession session){
+        Reply reply = new Reply();
+        //System.out.println("1:");
+        User user = userRepository.findByUsername((String)session.getAttribute("loginuser")).get(0);
+        //reply.content = content;
+        //String[] s = request.getParameterValues("rContent");
+        /*if(s == null) {
+            System.out.println("0");
+            return "redirect:/main";
+        }*/
+        if(rContent == null || rContent.length() == 0 )
+            return "redirect:/main";
+        System.out.println(rContent);
+        reply.content = rContent;
+        reply.userid = user.id;
+        reply.likes = 0;
+        Article article = articleRepository.findArticleById(Integer.parseInt(articleid));
+        reply.replyid = Integer.parseInt(replyid);
+        reply.articleid = article.id;
+        reply.create_time = new Date();
+        reply = replyRepository.save(reply);
+        System.out.println(reply.content);
+        return "redirect:/main";
 
+    }
 
 
 }
-
-
-        /*int reply1id = (int)session.getAttribute("reply");
-        if(reply1id != -1){
-            Reply reply1 = replyRepository.findReplyById((int)session.getAttribute("reply"));
-            reply.replyid = reply1.id;
-        }
-        else{*/
