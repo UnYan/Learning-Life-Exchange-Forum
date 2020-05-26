2e27b430519f4332c53e0c8ad3e45e06a7cbef1e
UnYan <415212688@qq.com>
2020-05-26 23:52:34 +0800

    '添加通知实体未完善'

---
+++ src/main/java/com/a/entity/Inform.java
@@ -0,0 +1,17 @@
+package com.a.entity;
+
+import javax.persistence.*;
+/*
+@Entity
+@Table(name = "inform")
+public class Inform{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int id;
+    @Column(name = "articleId")
+    public String articleId;
+    @Column(name = "userId")
+    public int userId;
+}
+*/
