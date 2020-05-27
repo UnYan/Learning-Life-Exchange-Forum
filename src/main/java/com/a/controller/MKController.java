deb88cc322876f339abb7e698e7ddf97ff732aed
nnddss <1076645646@qq.com>
2020-05-27 09:30:13 +0800

    课程推荐差不多了

--- src/main/java/com/a/controller/MKController.java
+++ src/main/java/com/a/controller/MKController.java
@@ -34,7 +34,7 @@ public class MKController {
     ReplyRepository replyRepository ;
     @Autowired
     UserRepository userRepository ;
-    String[] catrgory_name={"管理员帖子","帖子","校园周边","讨论区","题解"};
+    String[] catrgory_name={"管理员帖子","资源共享","校园周边","讨论区","题解","课程推荐"};
     @RequestMapping("/article/addArticle")
     public String upload(@RequestParam("title") String title,
                          @RequestParam("content") String content,
@@ -97,7 +97,7 @@ public class MKController {
     @GetMapping("/course")
     public String course(Model model) {
         Collection<Article> courses =articleRepository.findArticleByCategoryName("课程推荐");
-        model.addAttribute("courses", courses);
+        model.addAttribute("articles", courses);
         Collection<Reply> replys = replyRepository.findAll();
         model.addAttribute("replys",replys);
         return "home";
