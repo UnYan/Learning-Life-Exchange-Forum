22e9c56da04eeeebb2a743be49eb6d8e6dd8f8ef
XiXi1103 <1239174672@qq.com>
2020-05-31 21:04:46 +0800

    修复跳转以及通知的后端存储

--- src/main/java/com/a/entity/Article.java
+++ src/main/java/com/a/entity/Article.java
@@ -25,8 +25,14 @@ public class Article {
     @Column(name = "reply_cnt")
     public Integer reply_cnt;
     public String dateformat;
-    public Integer new_like;
-    public Integer new_like_id;
+    public Integer new_like;//这个属性是代表新增的赞的数量，
+    public Integer new_like_id;/*这个属性是最后一个点赞的用户id，我设想的通知栏是“xxx等几人赞了你的文章”，然后用户点击之后跳转到对应文章页面
+                            /*注意：用户点击该通知后，需要将两个newlike属性清零，代表此时没有新增点赞。清零涉及到update操作，和查找还不太一样，可以参考
+                            articleRepository当中的更新操作，然后目前这一版涉及到newlike和newreply的操作都在replyController中，有注释
+                            但目前点赞会出bug，原因在replycontroller当中，严某写的代码我没敢改，怕出现更多bug。。。。
+                            newreply的操作和点赞类似。
+                            现在需要做的就是从前端读取这些带new的属性然后显示在通知栏当中，并且点击后对new清零。加油，yzx与你同在！
+                            */
     public Integer new_reply;
     public Integer new_reply_id;
 //    @Column(name = "level")
