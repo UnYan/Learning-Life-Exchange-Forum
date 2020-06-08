0c6d88a0bfd586a43962f495f1bad20785e3e0cb
kxw009988 <15261398617@163.com>
2020-06-08 21:16:30 +0800

    管理员删除帖子

--- src/main/java/com/a/controller/ArticleDelController.java
+++ src/main/java/com/a/controller/ArticleDelController.java
@@ -7,6 +7,7 @@ import com.a.repository.ReplyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -28,6 +29,17 @@ public class ArticleDelController {
         List<Reply> relist = replyRepository.findByArticleid(id);
         replyRepository.deleteAll(relist);
         model.addAttribute("res", "删帖成功");
-        return "redirect:/";
+        return "editor/article";
+    }
+
+    @PostMapping(value = {"/delReply/{id}"})
+    public String deleteReply(@PathVariable("id") Integer id, Model model) {
+        Reply reply = replyRepository.findReplyById(id);
+        if (reply != null) {
+            replyRepository.delete(reply);
+            model.addAttribute("result", "控评成功");
+        }
+        else model.addAttribute("result", "控评失败");
+        return "/editor/article";
     }
 }
