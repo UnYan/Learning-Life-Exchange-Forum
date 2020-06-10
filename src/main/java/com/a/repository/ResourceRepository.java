98353693d52e4a8d423fa8da873244755891d149
kxw009988 <15261398617@163.com>
2020-06-10 10:57:10 +0800

    文件下载

---
+++ src/main/java/com/a/repository/ResourceRepository.java
@@ -0,0 +1,13 @@
+package com.a.repository;
+
+import com.a.entity.Resource;
+
+public class ResourceRepository {
+    public void add(Resource resource) {
+    }
+
+    public Resource findResourceById(Integer id) {}
+
+    public void delete(Resource resource) {
+    }
+}
